0b6b69a43015d094022fddde523cb6fa7c778719
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-02-16 23:04:19 -0500

    delete

--- src/main/java/com/robermejia/utp/controller/AdminController.java
+++ src/main/java/com/robermejia/utp/controller/AdminController.java
@@ -2,21 +2,29 @@ package com.robermejia.utp.controller;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.robermejia.utp.entities.Producto;
 import com.robermejia.utp.repositories.ProductoRepository;
+import com.robermejia.utp.services.ProductoService;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
 
     private final ProductoRepository productoRepository;
+
+    @Autowired
+    private ProductoService productoService;
 
     @GetMapping("/administracion")
     public String admin(Model model) {
@@ -38,6 +46,19 @@ public class AdminController {
             model.addAttribute("producto", producto);
             return "formulario-editar-productos";
         }
+        return "redirect:/administracion";
+    }
+
+    @PostMapping("/administracion/eliminar/{id}")
+    public String postMethodName(@PathVariable() String id, RedirectAttributes redirectAttrs) {
+        Optional<Producto> producto = productoRepository.findById(Integer.parseInt(id));
+        if(producto.isPresent()) {
+            productoService.deleteProducto(producto.get().getId());
+            redirectAttrs.addFlashAttribute("mensaje", "Producto eliminado con éxito");
+            return "redirect:/administracion";
+        }
+
+        redirectAttrs.addFlashAttribute("mensajeError", "Error, no encontro el producto");
         return "redirect:/administracion";
     }
 }
